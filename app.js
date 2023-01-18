8b6197377a893bf2ba17621a6c02dcdda46a4959
Shashank Singh <thakurshashank247@gmail.com>
2023-01-18 22:10:18 +0530

    inject react through cdn link then created a react element & injected inside the DOM.

---
+++ app.js
@@ -0,0 +1,32 @@
+console.log('Hello world')
+
+const heading1 = React.createElement(
+    "h1",
+    {
+        "id": "title"
+    },
+    "Heading 1"
+)
+const heading2 = React.createElement(
+    "h2",
+    {
+        "id": "title"
+    },
+    "Heading 2"
+)
+
+const container = React.createElement(
+    "div",
+    {
+        "id": "container"
+    },
+    [heading1, heading2]
+);
+
+console.log(container);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//passsing a react element inside the root
+
+root.render(container)
